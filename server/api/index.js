343b3526d44a82938aa765f1cf481901b74f8b7d
Wildan Rizky <wildanrizkiyadi68@gmail.com>
2023-01-05 22:51:48 +0700

    asdas

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,5 @@
 export default defineEventHandler(async(event) => {
-  const body = await readBody(event)
+  // const body = await readBody(event)
   const query = getQuery(event)
   return {
     test: 'Hellow ' + query.name,
